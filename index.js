f1ce8142cd9e39211681cb485b515a65d33460ce
sgarel <claussgeoffroy@gmail.com>
2016-03-24 19:58:21 +0000

    using connect and serve static

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
-
 var port = process.env.PORT || 5000;
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
-}).listen(port);
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
+});
