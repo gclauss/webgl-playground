b573818ad3a9d231135f36086f8058ca76f575a1
sgarel <claussgeoffroy@gmail.com>
2016-03-24 20:07:14 +0000

    fixed port

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var port = process.env.PORT || 5000;
 
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+connect().use(serveStatic(__dirname)).listen(port, function(){
+    console.log('Server running on ' + port + '...');
 });
