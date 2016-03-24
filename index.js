d437c9487c6484c0cc991bf44e50ac5c199b7593
sgarel <claussgeoffroy@gmail.com>
2016-03-24 19:55:25 +0000

    removed express and gone to simple http server

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-var express = require('express');
-var app = express();
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(index);
+}).listen(port);
